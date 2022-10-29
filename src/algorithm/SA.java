7de85c4de233698fefae055eb4da5cfa3bf0a5f2
test <email>
2022-10-29 17:04:35 +0800

    debug了增加m后的随机生成初始解的问题

--- src/algorithm/SA.java
+++ src/algorithm/SA.java
@@ -23,24 +23,25 @@ public class SA {
      * 每次选取之后就要收缩随机数的值域
      * @param route 一种解
      * @param m     垃圾车的数量
-     * 垃圾车数目 n = route.length - m - 1
+     * 垃圾车数目 n = route.length - m - 1, route.length = n + m - 1 + 2
      */
     public void build_random_sequence(int[] route, int m) {
         int n = route.length - m - 1;   //新建局部变量n,后面对n--不改变成员变量n的值
-        int[] numbers = new int[n];
+        int[] numbers = new int[route.length - 2];  //给route[1..倒数第二个]其中随机n个赋值为不同的[1..n], 其他全为0
          /*numbers[0..n-1]顺序存放[1..n]*/
         for (int i = 0; i < n; i++) {
             numbers[i] = i + 1;
         }
-        int n_temp = n;  //记录此时n的值, 作为循环长度, 不改变n_temp
-        for (int i = 1; i <= n_temp; i++) {
+        //int n_temp = n;  //记录此时n的值, 作为循环长度, 不改变n_temp
+        int j = numbers.length - 1;     //j 一开始指向numbers[]的末尾
+        for (int i = 1; i < (route.length - 1); i++) {
             //Math.random() :[0, 1) 的double
-            int r = (int) (Math.random() * n);  //r:[0..n - 1] 的整数
-            route[i] = numbers[r];          // 给rout[1..n]赋值为[1...n]的不重复的序列,
+            int r = (int) (Math.random() * numbers.length);  //r:[0..numbers.length - 1] 的整数
+            route[i] = numbers[r];          // 给rout[1..n + m - 1]赋值为[1...n]的不重复的序列,
             /**每次生成的下标是有可能重复的，但是由于numbers数组里该下标对应的值每抽中一次，
-             就被踢出去了（替换成了n-1位置上的值），所以最终结果并不会重复*/
-            numbers[r] = numbers[n - 1];
-            n--;
+             就被踢出去了（替换成了j位置上的值），所以最终结果并不会重复*/
+            numbers[r] = numbers[j];
+            j--;
         }
     }
 
